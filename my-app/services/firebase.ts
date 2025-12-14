c447593ed6eac670bf8bc1093140f0a62cb2ff76
Felipe Liandro <106708109+FleipeStark13@users.noreply.github.com>
2025-12-14 19:19:58 -0300

    Refactor Firebase config to use env variables
    Updated Firebase configuration to use environment variables.

--- my-app/services/firebase.ts
+++ my-app/services/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCs3MZa4FR9itEmek2ayG6JQ1sXHVq9X7A",
-  authDomain: "projectmk001.firebaseapp.com",
-  projectId: "projectmk001",
-  storageBucket: "projectmk001.firebasestorage.app",
-  messagingSenderId: "1014302885300",
-  appId: "1:1014302885300:web:a15d04a1434aa983b39ee1"
+  apiKey: process.env.NEXT_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
